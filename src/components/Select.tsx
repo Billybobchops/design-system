1ea8e20f574cc07b1120d7afd420765bd8b7463b
Billybobchops <chlanda52@gmail.com>
2024-08-06 14:53:09 -0400

    update select styling

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,5 +1,7 @@
 import classes from './Select.module.scss';
+import variables from '@/app/variables.module.scss';
 import { useId } from 'react';
+import { SelectChevron } from './Symbols';
 import Label from './Label';
 import InputHelperText from './InputHelperText';
 
@@ -36,22 +38,25 @@ const Select: React.FC<SelectProps> = ({
     return (
         <div className={classes.selectContainer}>
             <Label inputID={selectID} label={label} required={required} />
-            <select
-                aria-describedby={helperID}
-                aria-invalid={false}
-                className={classes.select}
-                disabled={disabled}
-                id={selectID}
-                name={name}
-                // onChange={handleChange}
-                required={required}
-			>
-                {options.map((option) => (
-                    <option key={option.value} value={option.value}>
-                        {option.value}
-                    </option>
-                ))}
-            </select>
+            <div className={classes.chevronPositioning}>
+                <select
+                    aria-describedby={helperID}
+                    aria-invalid={false}
+                    className={classes.select}
+                    disabled={disabled}
+                    id={selectID}
+                    name={name}
+                    // onChange={handleChange}
+                    required={required}
+				>
+                    {options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.value}
+                        </option>
+                    ))}
+                </select>
+                <SelectChevron fill={variables.icNeutral70} />
+            </div>
             <InputHelperText helperID={helperID} helperText={helperText} />
         </div>
     );
