b7c347310ccf94e2c8e2423d70fcedb31609961f
Billybobchops <chlanda52@gmail.com>
2024-12-13 17:08:21 -0500

    update chips

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -13,19 +13,18 @@ const Chip: React.FC<ChipProps> = ({ title, onRemove }) => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
-        <div className={classes.chip}>
-            <span className={classes.title}>{title}</span>
-            <button
-                onClick={onRemove}
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-			>
-                <span aria-hidden='true' className={classes.icon}>
-                    <MenuClose fill={isHovered ? variables.utilityNeutral0 : variables.themeA4} />
-                </span>
-                <VisuallyHidden>{`Remove ${title} as a selection`}</VisuallyHidden>
-            </button>
-        </div>
+		<button
+			className={`${classes.chip} ${isHovered ? classes.chipHover : ''}`}
+			onClick={onRemove}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
+			<span className={classes.title}>{title}</span>
+			<span aria-hidden='true' className={`${classes.icon} ${isHovered ? classes.iconHover : ''}`}>
+				<MenuClose fill={isHovered ? variables.utilityNeutral0 : variables.themeA4} />
+			</span>
+			<VisuallyHidden>{`Remove ${title} as a selection`}</VisuallyHidden>
+		</button>
     );
 };
 
